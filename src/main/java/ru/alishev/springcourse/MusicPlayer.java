b81cbaf76096c7c2a8c04e930095105ae92a72d7
K.Kshnyakin <kocmoc007@gmail.com>
2020-10-27 12:58:02 +0300

    Spring Framework. Урок 6: Внедрение зависимостей. Dependency Injection (DI). Часть 2.

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,14 +1,49 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private Integer volume;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer() {
+
+    }
+
+    public MusicPlayer(List<Music> music) {
+        this.musicList = music;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
