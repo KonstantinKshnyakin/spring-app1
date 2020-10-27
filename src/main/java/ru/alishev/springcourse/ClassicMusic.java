de64fd53c3605957009d5fca7fb4949d882b19e8
K.Kshnyakin <kocmoc007@gmail.com>
2020-10-27 15:53:13 +0300

    Spring Framework. Урок 8: Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- src/main/java/ru/alishev/springcourse/ClassicMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicMusic.java
@@ -2,6 +2,23 @@ package ru.alishev.springcourse;
 
 public class ClassicMusic implements Music {
 
+    private ClassicMusic() {
+        System.out.println("ClassicMusic initialize in constructor");
+    }
+
+    public static ClassicMusic getClassicalMusic() {
+        System.out.println("ClassicMusic initialize in factory methode");
+        return new ClassicMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("ClassicMusic-bean: doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("ClassicMusic-bean: doing my destroy");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -7,20 +7,27 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicMusic classicMusic1 = context.getBean("classicMusic", ClassicMusic.class);
+        ClassicMusic classicMusic2 = context.getBean("classicMusic", ClassicMusic.class);
+        ClassicMusic classicMusic3 = context.getBean("classicMusic", ClassicMusic.class);
+        System.out.println(classicMusic1.getSong());
+        System.out.println(classicMusic2.getSong());
+        System.out.println(classicMusic3.getSong());
 
-        System.out.println(firstMusicPlayer == secondMusicPlayer);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        secondMusicPlayer.setName("secondMusicPlayer");
-        secondMusicPlayer.setVolume(88);
-        firstMusicPlayer.playMusic();
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getName());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        System.out.println(firstMusicPlayer == secondMusicPlayer);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        secondMusicPlayer.setName("secondMusicPlayer");
+//        secondMusicPlayer.setVolume(88);
+//        firstMusicPlayer.playMusic();
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getName());
+//        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
