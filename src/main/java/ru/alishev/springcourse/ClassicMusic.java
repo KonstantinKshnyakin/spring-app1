7e75af0964d8cdd9583454b14ddf72b3d131402d
K.Kshnyakin <kocmoc007@gmail.com>
2020-10-27 16:25:20 +0300

    Spring Framework. Урок 9: Аннотации. Введение.

--- src/main/java/ru/alishev/springcourse/ClassicMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicMusic.java
@@ -1,5 +1,8 @@
 package ru.alishev.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicMusic implements Music {
 
     private ClassicMusic() {
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -2,32 +2,19 @@ package ru.alishev.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicMusic classicMusic1 = context.getBean("classicMusic", ClassicMusic.class);
-        ClassicMusic classicMusic2 = context.getBean("classicMusic", ClassicMusic.class);
-        ClassicMusic classicMusic3 = context.getBean("classicMusic", ClassicMusic.class);
-        System.out.println(classicMusic1.getSong());
-        System.out.println(classicMusic2.getSong());
-        System.out.println(classicMusic3.getSong());
+        Music musicBean1 = context.getBean("rockMusic", Music.class);
+        Music musicBean2 = context.getBean("classicMusic", Music.class);
+        Music musicBean3 = context.getBean("rapMusic", Music.class);
 
-//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        System.out.println(firstMusicPlayer == secondMusicPlayer);
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        secondMusicPlayer.setName("secondMusicPlayer");
-//        secondMusicPlayer.setVolume(88);
-//        firstMusicPlayer.playMusic();
-//        System.out.println(firstMusicPlayer.getName());
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getName());
-//        System.out.println(secondMusicPlayer.getVolume());
+        MusicPlayer musicPlayer = new MusicPlayer(Arrays.asList(musicBean1, musicBean2, musicBean3));
+        musicPlayer.playMusic();
 
         context.close();
     }
