8dbb3d666e3d087a057a5ca7b64dadde79eadf6e
K.Kshnyakin <kocmoc007@gmail.com>
2020-11-25 23:11:51 +0300

    подготовка к переносу попытка перенести проект как модуль ы другое проект

--- spring-app-alishev/src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ spring-app-alishev/src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 @Component
